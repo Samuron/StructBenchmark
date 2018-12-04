ed4744bac85dceb84c060da7268e696fabbbdfe8
Ievgen Makukh <ievgen_makukh@epam.com>
2018-12-04 09:28:14 +0200

    Initial commit

---
+++ StructBenchmark/StructBenchmark/Program.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Utf8Json;
+
+namespace StructBenchmark
+{
+    public struct JsonMessage
+    {
+        public JsonMessage(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; set; }
+    }
+
+    [DisassemblyDiagnoser]
+    public class JsonBenchmark
+    {
+        private const string Text = "Hello, World!";
+
+        [Benchmark(Baseline = true)]
+        public void Initializer()
+        {
+            var json = JsonSerializer.SerializeUnsafe(new JsonMessage { Message = Text });
+        }
+
+        [Benchmark]
+        public void Constructor()
+        {
+            var json = JsonSerializer.SerializeUnsafe(new JsonMessage(Text));
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<JsonBenchmark>();
+        }
+    }
+}
